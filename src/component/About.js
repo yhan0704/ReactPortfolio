8e734cb1529281e299d2498eb66e45a445c9f9eb
github username <hanyc1205@gmail.com>
2020-07-12 22:22:56 -0400

    added about me

--- src/component/About.js
+++ src/component/About.js
@@ -13,19 +13,26 @@ export default function About() {
         </div>
         <p>Hi, My name is Young Chan Han.</p>
         <p>
-          I am a driven full-stack engineer with an outstanding work ethic
-          seeking a junior level developer position. 
-          I am also passionate about coding because of my drive to solve problems and perfect a final product. 
+          Hi, My name is Young Chan Han. I am a driven fu ll-stack engineer with
+          an outstanding work ethic open to contributing my diverse skill set in
+          a junior-level position.
         </p>
         <p>
-          I've recently completed FlatIron School, 15months web development
-          bootcamp.           
-          I've built web applications using Redux, React, Ruby and Ruby on Rails
-          programming.
+          I hold an Associate’s Degree in Computer Science and recently
+          graduated from the University of Maryland, Baltimore County with a
+          Bachelor of Science Degree in Information Systems. From there I
+          attended Flatiron School where I honed my skills as a developer.
+          During FlatIron School’s 15-Week Intensive Software Engineering
+          Program, I’ve built web applications using Redux, React, Ruby, and
+          Ruby on Rails programming.
         </p>
         <p>
-          Performed a wide range of design and coding projects utilizing HTML,
-          CSS, API, and JavaScript.
+          I am passionate about coding because of my drive to solve problems and
+          create a final product. Beyond my academic qualifications, I continue
+          to advance my skills as a developer by enhancing projects within my
+          portfolio and preparing for the AWS practitioner certification. I
+          welcome you to take a look at my portfolio and please contact me with
+          any questions you may have.
         </p>
       </div>
     </div>
